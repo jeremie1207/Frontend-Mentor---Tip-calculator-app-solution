815301053fd6dc6ee1677b0dfec14d58f43c1ec1
jeremie1207 <jbarh086@uottawa.ca>
2025-11-09 18:19:18 -0500

    implement error

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const form = document.getElementById("form");
 const resetBtn = document.getElementById("btn-reset");
 const tips = document.querySelectorAll("input[name='tip']");
 const btnTips = document.querySelectorAll(".btn-tip");
 const customTipInput = document.getElementById("custom-tip");
-
-console.log(btnTips);
+const billAmountInput = document.getElementById("bill");
+const numberOfPeople = document.getElementById("people");
+const errorMessage = document.querySelector(".error-message");
+let tipPercent;
 
 const removeDataStateOnBtnTips = () => {
   btnTips.forEach((btn) => {
@@ -18,9 +21,51 @@ const setActive = (id) => {
 
 tips.forEach((tip) => {
   tip.addEventListener("change", function () {
+    customTipInput.value = "";
     if (this.checked) {
-      console.log(`Radio button with value "${this.id}" is now checked.`);
       setActive(this.id);
+      tipPercent = parseInt(this.value);
+      console.log(tipPercent);
     }
   });
+});
+
+customTipInput.addEventListener("click", function () {
+  removeDataStateOnBtnTips();
+  tips.forEach((tip) => {
+    tip.checked = false;
+  });
+});
+
+customTipInput.addEventListener("change", function (event) {
+  const tip = parseInt(event.target.value);
+  if (!(tip > 0 && tip <= 100)) {
+    event.target.setAttribute("aria-invalid", "true");
+    return;
+  }
+  event.target.setAttribute("aria-invalid", "false");
+  tipPercent = tip;
+  console.log(tipPercent);
+});
+
+billAmountInput.addEventListener("change", function (event) {
+  const bill = parseInt(event.target.value);
+  if (!(bill > 0)) {
+    event.target.setAttribute("aria-invalid", "true");
+    event.target.value = null;
+    return;
+  }
+  event.target.setAttribute("aria-invalid", "false");
+});
+
+numberOfPeople.addEventListener("change", function (event) {
+  const amount = parseInt(event.target.value);
+  if (!(amount > 0)) {
+    event.target.setAttribute("aria-invalid", "true");
+    event.target.value = null;
+    errorMessage.style.display = "block";
+    return;
+  }
+  event.target.setAttribute("aria-invalid", "false");
+  errorMessage.style.display = "none";
 });
